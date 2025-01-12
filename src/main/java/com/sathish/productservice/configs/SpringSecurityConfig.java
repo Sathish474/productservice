38bbbc824b95318305dc5d01e0db2a75017644ad
Sathish474 <sathishchowdary474@gmail.com>
2025-01-12 17:51:25 -0600

    Implemented MVC Controller Unit test cases for product service using @WebMvcTest

--- src/main/java/com/sathish/productservice/configs/SpringSecurityConfig.java
+++ src/main/java/com/sathish/productservice/configs/SpringSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.sathish.productservice.configs;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
--- src/test/java/com/sathish/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/sathish/productservice/ProductserviceApplicationTests.java
@@ -1,15 +1,55 @@
 /*
 package com.sathish.productservice;
 
+import com.sathish.productservice.controllers.ProductController;
+import com.sathish.productservice.dto.products.CreateProductRequestDto;
+import com.sathish.productservice.dto.products.CreateProductResponseDto;
+import com.sathish.productservice.models.Product;
+import com.sathish.productservice.services.ProductService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(ProductController.class)
+@ActiveProfiles("test")
 class ProductserviceApplicationTests {
 
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService productService;
+
     @Test
-    void contextLoads() {
+    public void Test_CreateProduct_With_Valid_RequestBody() {
+        // Arrange
+        when(productService.createProduct(any())).thenReturn(new Product());
+        // Act
+        CreateProductResponseDto response = productController.createProduct(new CreateProductRequestDto());
+        // Assert
+        assert response != null;
+        assertEquals(CreateProductResponseDto.class, response.getClass());
     }
+
+    */
+/*
+    public CreateProductResponseDto createProduct(@RequestBody CreateProductRequestDto createProductRequestDto) {
+        logger.info("Creating product with name: {}", createProductRequestDto.toString());
+        Product product = productService.createProduct(createProductRequestDto.toProduct());
+        logger.info("Product created with id: {}", product.getId());
+        return CreateProductResponseDto.from(product);
+    }
+     *//*
 
 }
 */
